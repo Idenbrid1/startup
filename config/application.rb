8d799b0817bb5d302d5d2a16553525c199ab2efd
Idenbrid1 <login.idenbrid@gmail.com>
2020-12-29 00:13:44 +0500

    who

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-require "active_storage/engine"
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -8,14 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Startup
   class Application < Rails::Application
-  	config.railties_order = [ActiveStorage::Engine, :main_app, :all]
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.generators do |g|
-      g.orm             :active_record
-      g.template_engine :erb
-      g.test_framework  :test_unit, fixture: true
-    end
     config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
